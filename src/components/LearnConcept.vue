afa32f5cdc7b5edf97460cac06abcd1aaaa990dc
y.ueki <pirori67@gmail.com>
2019-12-14 22:15:22 +0900

    Conditional branch if using data

---
+++ src/components/LearnConcept.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="show">Hello vue concept</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
