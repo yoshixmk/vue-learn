193b806b3a4418828baa234425b33e96bebca62c
YoshihiroUeki <pirori67@gmail.com>
2019-12-21 22:57:31 +0900

    disapear v-cloak after wait showing

--- src/components/LearnTemplate.vue
+++ src/components/LearnTemplate.vue
@@ -4,6 +4,10 @@
     <a v-bind="href" v-pre>Hello {{message}}</a>
     <!-- once compile, and show -->
     <a v-bind:href="url" v-once>Hello {{message}}</a>
+
+    <span v-text="message"></span>
+    <span v-html="tag"></span>
+    <div v-cloak>v-cloak {{message}}</div>
   </div>
 </template>
 
@@ -12,7 +16,8 @@ export default {
   data() {
     return {
       url: 'localhost:8080',
-      message: 'first message'
+      message: 'first message',
+      tag: 'Hello <strong>Vue.js!</strong>'
     }
   }
 
@@ -20,5 +25,5 @@ export default {
 </script>
 
 <style scoped>
-
+  [v-cloak] {display: none}
 </style>
