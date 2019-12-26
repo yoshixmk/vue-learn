57c1a335bec159a1d31e21a9589387052c41afd7
YoshihiroUeki <pirori67@gmail.com>
2019-12-26 22:35:07 +0900

    computed rand only cached value

--- src/components/LearnComputed.vue
+++ src/components/LearnComputed.vue
@@ -3,6 +3,10 @@
     <p>{{width}}のはんぶんは{{halfWidth}}</p>
     <input v-model.number="width">
     <input v-model.number="halfWidth">
+    <p>diff methodRand {{methodRand()}}</p>
+    <p>diff methodRand {{methodRand()}}</p>
+    <p>same computedRand {{computedRand}}</p>
+    <p>same computedRand {{computedRand}}</p>
   </div>  
 </template>
 
@@ -13,11 +17,17 @@ export default {
       width: 400
     }
   },
+  methods: {
+    methodRand: function(){return Math.random()}
+  },
+  // method: との違いとして、キャッシュ機能がある。
+  // リアクティブなデータに使わなかった場合は、同じものが返る
   computed: {
     halfWidth: {
       get: function() {return this.width / 2},
       set: function(val) {this.width = val * 2}
-    }
+    },
+    computedRand: function(){return Math.random()}
   }
 }
 </script>
