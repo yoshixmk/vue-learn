b0d16eb5572796d644f5b5ab515ecd7dc74b0d82
y.ueki <pirori67@gmail.com>
2020-01-01 19:43:22 +0900

    directives fook

--- src/components/ConvertText.vue
+++ src/components/ConvertText.vue
@@ -1,7 +1,10 @@
 <template>
+  <div>
     <p>
         180 度 = {{ 180 | radian | round }} ラジアン
     </p>
+    <input type="text" v-focus>
+  </div>
 </template>
 
 <script>
@@ -14,6 +17,15 @@ export default {
     // 度からラジアンに変換するフィルタ
     radian: function (val) {
       return val * Math.PI / 180
+    }
+  },
+  directives: {
+    focus: {
+      // 紐付いている要素がDOMに挿入されるとき
+      inserted: function (el) {
+        el.focus() // 要素にフォーカスを当てる
+      }
+      // insertedの他、bind, update, componentUpdated, unbindのフックがある
     }
   }
 }
