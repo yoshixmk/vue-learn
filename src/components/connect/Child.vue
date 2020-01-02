f9e22661e3cc2ea2197544cbb507af9a1eb254b3
y.ueki <pirori67@gmail.com>
2020-01-02 23:17:21 +0900

    add child file

---
+++ src/components/connect/Child.vue
@@ -0,0 +1,22 @@
+<template>
+    <li>{{ name }} HP.{{ hp }}
+    <button v-on:click="doAttack">攻撃する</button></li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    name: String,
+    hp: Number
+  },
+  methods: {
+    // ボタンのクリックイベントのハンドラから$emitでattackを発火する
+    doAttack: function () {
+      // 引数として自分のIDを渡す
+      this.$emit('attack', this.id)
+    }
+  }  
+}
+</script>
+  
