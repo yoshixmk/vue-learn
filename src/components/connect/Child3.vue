6ea8f983a83896e1f7fcf5fc3ab4c22dfbbae833
YoshihiroUeki <pirori67@gmail.com>
2020-01-06 23:30:14 +0900

    props and model bindings

---
+++ src/components/connect/Child3.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="my-component">
+  <p>名前.{{ name }} HP.{{ hp }}</p>
+  <p>名前 <input v-model="localName"></p>
+  <p>HP <input size="5" v-model.number="localHp"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    hp: Number
+  },
+  computed: {
+    // 算出プロパティのセッター＆ゲッターを使ってv-modelを使用
+    localName: {
+      get() { // get: function() {}と同じ
+        return this.name
+      },
+      set(val) { // set: function(val) {}と同じ
+        this.$emit('update:name', val)
+      }
+    },
+    localHp: {
+      get() {
+        return this.hp
+      },
+      set(val) {
+        this.$emit('update:hp', val)
+      }
+    }
+  }
+}
+</script>
--- src/components/connect/Parent.vue
+++ src/components/connect/Parent.vue
@@ -4,6 +4,7 @@
             v-bind:key="item.id"
             v-bind="item"
             v-on:attack="handleAttack"></Child>
+
         <Child2>
             <header slot="header">
                 Hello Vue.js!
@@ -13,17 +14,23 @@
               footer Vue.js!
             </footer>
         </Child2>
+
+        <!-- :nameはv-bind:nameと同じ -->
+        <Child3 :name.sync="name" :hp.sync="hp">
+        </Child3>
     </ul>    
 </template>
 
 <script>
 import Child from './Child'
 import Child2 from './Child2'
+import Child3 from './Child3'
 
 export default {
   components: {
     Child,
-    Child2
+    Child2,
+    Child3
   },
   data: function() {
     return{
@@ -31,7 +38,9 @@ export default {
         { id: 1, name: 'スライム', hp: 100 },
         { id: 2, name: 'ゴブリン', hp: 200 },
         { id: 3, name: 'ドラゴン', hp: 500 }
-      ]
+      ],
+      name: 'スライム',
+      hp: 100
     }
   },
   methods: {
