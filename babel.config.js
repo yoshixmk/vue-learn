401bcbeea7b7f3b456e485dbfe939d9b67caec7d
YoshihiroUeki <pirori67@gmail.com>
2020-01-09 21:52:21 +0900

    add corejs 3 for polyfil

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: { version: 3, proposals: true }
+      }
+    ]
   ]
 }
