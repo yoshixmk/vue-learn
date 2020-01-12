086b0aa2b91c9b7baa1a31cdc26668acf91d4b6b
y.ueki <pirori67@gmail.com>
2020-01-12 21:00:24 +0900

    do update using v-bind&event or v-model

---
+++ src/components/EditForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="edit-form">
+    <h3>バインドとイベントを使った場合</h3>
+    <input type="text" :value="message" @input="doUpdate">
+    <h3>v-model を使った場合</h3>
+    <input v-model="message2">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditForm',
+  computed: {
+    message() {
+      return this.$store.getters.message
+    },
+    message2: {
+      get() { return this.$store.getters.message },
+      set(value) { this.$store.dispatch('doUpdate', value) }
+    }
+  },
+  methods: {
+    doUpdate(event) {
+      // input の値を持ってディスパッチ
+      this.$store.dispatch('doUpdate', event.target.value)
+    }
+  }
+}
+</script>
--- src/components/LearnStore.vue
+++ src/components/LearnStore.vue
@@ -1,9 +1,15 @@
 <template>
-  <p>{{ message }}</p>
+  <div>
+    <p>{{ message }}</p>
+    <EditForm></EditForm>
+  </div>
 </template>
 
 <script>
+import EditForm from './EditForm'
+
 export default {
+  components: { EditForm },
   created() {
     console.log(this.$store.state.count) // eslint-disable-line
     this.$store.commit('increment')
