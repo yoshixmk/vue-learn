83937afb8b45a0b84c45bb21f7edef7a09f00964
YoshihiroUeki <pirori67@gmail.com>
2020-01-14 22:26:48 +0900

    mapping uri in router

--- src/router.js
+++ src/router.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
-
 import VueRouter from 'vue-router'
+import Home from '@/views/Home'
+import Product from '@/views/Product'
 
 Vue.use(VueRouter)
+
+// VueRouterインスタンスを生成する
+const router = new VueRouter({
+  // URLのパスと紐づくコンポーネントをマッピング
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/product',
+      component: Product
+    }
+  ]
+})
+export default router
