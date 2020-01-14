e636f43a768699a5ae271d0d6ccfaedffcfe0aa2
YoshihiroUeki <pirori67@gmail.com>
2020-01-14 22:33:52 +0900

    spa router view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <nav>
+      <router-link to="/"> Home </router-link>/
+      <router-link to="/product"> Product </router-link>
+    </nav>
+    <router-view />
     <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!-- <LearnConcept/>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import 'core-js'
 import Vue from "vue";
 import App from "./App.vue";
 import store from '@/store.js';
+import router from '@/router.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  el: '#app',
   store,
+  router,
   render: h => h(App)
-}).$mount("#app");
+})
