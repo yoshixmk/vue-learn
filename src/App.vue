8efd0659657d094bb13c63869a0af3d7f58835fc
YoshihiroUeki <pirori67@gmail.com>
2020-01-21 01:01:08 +0900

    showing products contents using watch

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav>
       <router-link to="/"> Home </router-link>/
-      <router-link to="/product"> Product </router-link>
+      <router-link to="/products"> Product </router-link>
     </nav>
     <router-view />
     <img alt="Vue logo" src="./assets/logo.png" />
